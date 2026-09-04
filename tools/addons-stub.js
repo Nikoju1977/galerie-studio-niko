d487e0bf9e79c4cc29b0f622965459d3823a40d6
Nikoju1977 <nicolasjulienne.nj@gmail.com>
2026-09-04 15:18:02 +0200

    Simulateur des modules three additionnels

---
+++ tools/addons-stub.js
@@ -0,0 +1,6 @@
+const mk = () => new Proxy(function(){}, {
+  get(t,k){ if(k==='then') return undefined; if(!(k in t)) t[k]=mk(); return t[k]; },
+  set(t,k,v){ t[k]=v; return true; }, apply(){ return mk(); }, construct(){ return mk(); }
+});
+export const OrbitControls=mk(), RoomEnvironment=mk(), Reflector=mk(), EffectComposer=mk(),
+  RenderPass=mk(), UnrealBloomPass=mk(), OutputPass=mk(), GLTFLoader=mk();
